c2e2f9d706bbb322090989b17fe529b7a34af1c9
Mikulius <bucior.te@gmail.com>
2023-04-23 21:18:30 +0200

    okno yhym

---
+++ teatr/teatr/Form1.cs
@@ -0,0 +1,15 @@
+namespace teatr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World";
+        }
+    }
+}
