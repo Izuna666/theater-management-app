f1ff8e5d32477fe364353d642025798cdbdf7ed9
Mikulius <bucior.te@gmail.com>
2023-05-29 20:55:32 +0200

    yhym2

---
+++ teatr/teatr/View/LoginView.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+
+
+namespace okno_logowania.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string user= txtUserName.Text;
+            string password = txtUserPassword.Password;
+            string connectionString = "server=localhost;port=3306;database=Login;uid=root;password=yhym2137;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            try
+            {
+                String querry = "SELECT * FROM login WHERE Username = '" + txtUserName.Text + "' AND Password = '"+txtUserPassword.Password+"'";
+                MySqlDataAdapter sda = new MySqlDataAdapter(querry, connection);
+
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                if(dt.Rows.Count > 0)
+                {
+                    user = txtUserName.Text;
+                    password = txtUserPassword.Password;
+
+                    MainWindow objMainWindow = new MainWindow();
+                    //this.Visibility = Visibility.Hidden;
+                    objMainWindow.Show();
+                    this.Close();
+                }
+
+                else
+                {
+                    invalidLogin.Text = "invalid login or password, try again";
+                    txtUserName.Clear();
+                    txtUserPassword.Clear();
+
+                    txtUserName.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            /*if (txtUserName.Text == "admin" & txtUserPassword.Password == "1234")
+            {
+                MainWindow objMainWindow = new MainWindow();
+                //this.Visibility = Visibility.Hidden;
+                objMainWindow.Show();
+                this.Close();
+            }
+            else
+                invalidLogin.Text = "invalid login or password, try again";*/
+
+        }
+    }
+}
---
+++ teatr/teatr/View/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace okno_logowania.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtGodzina.Focus();
+            LoadDataIntoGrid();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            LoginView objloginView = new LoginView();
+            objloginView.Show();
+            this.Close();
+
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            string connectionString = "server=localhost;port=3306;database=Login;uid=root;password=yhym2137;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                String querry = "insert into repertuar (godzina,sala,nazwa,liczbaMiejsc,cena) values('"+this.txtGodzina.Text+"','"+this.txtSala.Text+"','"+this.txtNazwa.Text+"','"+this.txtLiczba.Text+"','"+this.txtCena.Text+"')";
+                MySqlCommand createCommand = new MySqlCommand(querry, connection);
+                createCommand.ExecuteNonQuery();
+                ClearText();
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                connection.Close();
+                LoadDataIntoGrid();
+            }
+        }   
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+
+            string connectionString = "server=localhost;port=3306;database=Login;uid=root;password=yhym2137;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                String querry = "delete from repertuar where godzina='"+this.txtGodzina.Text+"'";
+                MySqlCommand createCommand = new MySqlCommand(querry, connection);
+                createCommand.ExecuteNonQuery();
+                
+                ClearText();
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                connection.Close();
+                LoadDataIntoGrid();
+            }
+        }
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            LoadDataIntoGrid();
+            /*string connectionString = "server=localhost;port=3306;database=Login;uid=root;password=yhym2137;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            try
+            {
+                String querry = "SELECT godzina,sala,nazwa,liczba miejsc,cena FROM repertuar  ";
+                MySqlCommand createCommand = new MySqlCommand(querry, connection);
+                createCommand.ExecuteNonQuery();
+
+
+                MySqlDataAdapter sda = new MySqlDataAdapter(createCommand);
+                DataTable dt = new DataTable("repertuar");
+                sda.Fill(dt);
+                dataGrid1.ItemsSource = dt.DefaultView;
+                sda.Update(dt);
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                connection.Close();
+            }*/
+        }
+        private void LoadDataIntoGrid()
+        {
+            string connectionString = "server=localhost;port=3306;database=Login;uid=root;password=yhym2137;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            String querry = "SELECT godzina,sala,nazwa,liczbaMiejsc,cena FROM repertuar  ";
+            MySqlCommand createCommand = new MySqlCommand(querry, connection);
+            createCommand.ExecuteNonQuery();
+
+
+            MySqlDataAdapter sda = new MySqlDataAdapter(createCommand);
+            DataTable dt = new DataTable("repertuar");
+            sda.Fill(dt);
+            dataGrid1.ItemsSource = dt.DefaultView;
+            sda.Update(dt);
+            connection.Close();
+
+        }
+        private void ClearText()
+        {
+            txtGodzina.Clear();
+            txtSala.Clear();
+            txtNazwa.Clear();
+            txtLiczba.Clear();
+            txtCena.Clear();
+        }
+    }
+}
