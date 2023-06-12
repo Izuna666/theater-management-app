f98c30c0afb6c9b6aa9c37c322ad6ce800a131ca
Mikulius <bucior.te@gmail.com>
2023-06-12 07:35:48 +0200

    plik klasa

---
+++ teatr/teatr/View/Repertuar.cs
@@ -0,0 +1,77 @@
+﻿using MySql.Data.MySqlClient;
+using okno_logowania.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Repertuar
+{
+    /*public bool IsSelected { get; set; }*/
+    public int ID { get; set; }
+    public string? Godzina { get; set; }
+    public string? Sala { get; set; }
+    public string? Nazwa { get; set; }
+    public string? LiczbaMiejsc { get; set; }
+    public string? Cena { get; set; }
+}
+
+public class RepertuarRepository
+{
+    private string connectionString = "server=localhost;port=3306;database=Login;uid=root;password=yhym2137;";
+
+    public void AddRepertuar(Repertuar repertuar)
+    {
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO repertuar ( godzina, sala, nazwa, liczbaMiejsc, cena) " +
+                           $"VALUES ('{repertuar.Godzina}', '{repertuar.Sala}', '{repertuar.Nazwa}', '{repertuar.LiczbaMiejsc}', '{repertuar.Cena}')";
+            MySqlCommand createCommand = new MySqlCommand(query, connection);
+            createCommand.ExecuteNonQuery();
+        }
+    }
+
+    public void RemoveRepertuar(int ID)
+    {
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = $"DELETE FROM repertuar WHERE ID = {ID}";
+            MySqlCommand createCommand = new MySqlCommand(query, connection);
+            createCommand.ExecuteNonQuery();
+        }
+    }
+
+    public List<Repertuar> GetRepertuarList()
+    {
+        List<Repertuar> repertuarList = new List<Repertuar>();
+
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT ID, godzina, sala, nazwa, liczbaMiejsc, cena FROM repertuar";
+            MySqlCommand createCommand = new MySqlCommand(query, connection);
+            using (MySqlDataReader reader = createCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Repertuar repertuar = new Repertuar()
+                    {
+                        ID = reader.GetInt16("ID"),
+                        Godzina = reader.GetString("godzina"),
+                        Sala = reader.GetString("sala"),
+                        Nazwa = reader.GetString("nazwa"),
+                        LiczbaMiejsc = reader.GetString("liczbaMiejsc"),
+                        Cena = reader.GetString("cena")
+                    };
+                    repertuarList.Add(repertuar);
+                }
+            }
+        }
+
+        return repertuarList;
+    }
+}
